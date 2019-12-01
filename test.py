5ab7fe3a2bd679ac5b93bc14dd0da089b0ebbf61
cyndi088 <cyndi088@163.com>
2019-12-01 21:59:53 +0800

    django-rest

---
+++ test.py
@@ -0,0 +1,10 @@
+from tutorial.snippets.models import Snippet
+from tutorial.snippets.serializers import SnippetSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+snippet = Snippet(code='foo = "bar"\n')
+snippet.save()
+
+snippet = Snippet(code='print("hello, world")\n')
+snippet.save()
